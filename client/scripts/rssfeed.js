441eed5ddc51b72168b55e5c1614d7df35f80d6b
Richard John Gascoigne <richard.john.gascoigne@gmail.com>
2013-05-30 16:03:28 +0200

    Styled up form and added more Jquery on $.get
    Split out url with query string on RSS feed
    Added try catch for any missing items

--- client/scripts/rssfeed.js
+++ client/scripts/rssfeed.js
@@ -1,20 +1,58 @@
 $(document).ready(function (){
 $('.btnsubmit input').click(function(){	
-	$('#newsfeed').each(function(){
+	$('#newsfeedresult').each(function(){
 		var $container = $(this);
 		$container.empty();
 		
-		var feedUrl = $('#urlfeed').val();
+		var feedUrl = $('#urlProxy').val();
+		feedUrl+= '?uri=';
+		feedUrl+= $('#queryStringUrl').val();
+		
 		
 		$.get(feedUrl, function(data) {
 		    var $xml = $(data);
+		    
+		    try{
+			    var $items = $xml.find('item');
+			    
+			    $items.each(function(){
+			       	try
+			       	{
+				    	var $headline = $('<h4>Headline here..</h4>').append($('title', this).text());
+				    	var $description = $('<p>Headline here..</p>').append($('description', this).text());
+				    	var $textlink = $('<a></a>')
+				    		.attr('href', $('link', this).text())
+			    			.text($('title', 'story link'));
+//				    	var $img1 = $('<img></img>')
+//			    		.attr('src', $('media:thumbnail', this).firstchild.('url').text())
+//		    			.text($('title', 'image'));
+						$('<div></div>')
+							.append($headline)
+							.append($description)
+							.append($textlink)
+	//						.append($img1)
+							.appendTo($container);
+			       	}
+			       	catch(exception){
+						var $headline = $('<h4>Item empty</h4>').append($xml);
+						$('<div>No item here.</div>')
+							.append($headline)
+							.appendTo($container);			       		
+			       	}
+				});
+		    }
+		    catch(exception)
+		    {
 				var $headline = $('<h4>Headline here..</h4>').append($xml);
-				$('<div></div>')
+				$('<div>No items found in this feed..</div>')
 					.append($headline)
 					.appendTo($container);
-			});
+		    }
+		    
 		});	
-	});	
+	});
 });
+});
+
 
 
